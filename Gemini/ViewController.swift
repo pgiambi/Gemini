97f86395d0d11477f698b47553f7ca6b3ee531db
Paolo Giambi <paologiambi@10-20-50-31.0200.fed.it.iosda.org>
2018-10-15 09:53:51 +0200

    Initial Commit

---
+++ Gemini/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gemini
+//
+//  Created by Paolo Giambi on 12/10/2018.
+//  Copyright © 2018 Paolo Giambi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
