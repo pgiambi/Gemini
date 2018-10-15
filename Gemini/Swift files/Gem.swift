4df2a5af64dfb11a0ce15ca6110b9f801e381845
Paolo Giambi <paologiambi@10-20-50-31.0200.fed.it.iosda.org>
2018-10-15 12:03:34 +0200

    Add the controllers cocoa touch files

---
+++ Gemini/Swift files/Gem.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Gemini
+//
+//  Created by Paolo Giambi on 15/10/2018.
+//  Copyright © 2018 Paolo Giambi. All rights reserved.
+//
+
+import Foundation
+
+struct Gem {
+    var name: String
+    var info: String?
+}
---
+++ Gemini/Swift files/gemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GemViewController.swift
+//  Gemini
+//
+//  Created by Paolo Giambi on 15/10/2018.
+//  Copyright © 2018 Paolo Giambi. All rights reserved.
+//
+
+import UIKit
+
+class gemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
